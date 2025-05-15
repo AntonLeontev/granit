38bb24834cfd7b0d537414832f287983ab4a6fd9
Anton <aner-anton@yandex.ru>
2025-05-15 17:57:30 +0300

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use App\Models\CivilProduct;
+use App\Models\MilitaryProduct;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,346 +14,142 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $position = CivilProduct::max('position');
-        CivilProduct::create([
+        $position = MilitaryProduct::max('position');
+        MilitaryProduct::create([
             'position' => ++$position,
-            'title_ru' => 'Радиолокационная станция «ШТОРМ»',
-            'title_en' => 'Radar station “STORM”',
-            'short_title_ru' => 'РЛС «ШТОРМ»',
-            'short_title_en' => 'Radar “STORM”',
-            'description_ru' => '<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>Радиолокационная станция &laquo;ШТОРМ&raquo; предназначена для морских судов, в том числе с водоизмещением свыше 10 000 тон. Может устанавливаться для замены устаревших РЛС в качестве отдельного рабочего места или быть встроена в интегрированную мостиковую систему современных кораблей, как на этапе проектирования и строительства, так и во время эксплуатации.&nbsp;Также возможна установка на скоростные корабли.</div>
-<div>&nbsp;</div>
-<div>Модульная конструкция РЛС обеспечивает простой и быстрый монтаж.&nbsp;Техобслуживание фактически не требуется на всём сроке эксплуатации.&nbsp;Возможна оперативная замена любого электронного узла, без пайки и&nbsp;специального инструмента в течении 2х часов. Имеется возможность&nbsp;установки запретных зон.</div>
-<div>&nbsp;</div>
-<div>Может применяться в том числе для мониторинга ледовых полей.&nbsp;Допускается применение в качестве стационарной береговой РЛС, в том&nbsp;числе в условиях Арктики.</div>
-<div>&nbsp;</div>
-<div>Комплект поставки включает в себя РЛС, построенную на антеннах&nbsp;различной длины (4, 6 или 8 футов), индикатор, пульт управления и блок&nbsp;обработки.</div>
-<div>&nbsp;</div>
-<div>Применение РЛС &laquo;ШТОРМ&raquo; позволит обеспечить технологический&nbsp;суверенитет и уменьшить количество импортного оборудования в&nbsp;отечественном судостроении.</div>
-</div>',
-            'description_en' => '<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>Radar station &ldquo;STORM&rdquo; is intended for sea vessels, including with displacement&nbsp;over 10 000 tons. It may be installed in replacement of obsolete radars as a&nbsp;separate workplace or may be embedded into the integrated bridge system of&nbsp;modern vessels as on the stage of design and construction, as well as on the stage&nbsp;of operation. It also may be installed on high-speed vessels.</div>
-<div>&nbsp;</div>
-<div>Modular structure of radar provides easy and fast mounting. Technical service&nbsp;practically is not required during the whole service life. Operative replacement of&nbsp;any electronic unit is possible, without soldering and special instruments in the&nbsp;period of 2 hours. There is a possibility to set the restricted areas.</div>
-<div>&nbsp;</div>
-<div>The radar could be implemented for monitoring the ice fields. It is possible to&nbsp;use it as a stationary coastal radar, including the usage in Arctic region.</div>
-<div>&nbsp;</div>
-<div>The set of supply comprises of the radar, built on various length antennas (4, 6&nbsp;or 8 feet), the indicator, control console and processing unit.</div>
-</div>',
-            'characteristics_ru' => '<table style="border-collapse: collapse; width: 100%; height: 268.8px;" border="1"><colgroup><col style="width: 50%;"><col style="width: 50%;"></colgroup>
+            'title_ru' => 'РАДИОЛОКАЦИОННАЯ СИСТЕМА КРМ-66Э',
+            'title_en' => 'RADAR SYSTEM KRM-66E',
+            'short_title_ru' => 'РЛС КРМ-66Э',
+            'short_title_en' => 'Radar KRM-66E',
+            'characteristics_ru' => '<table style="border-collapse: collapse; width: 100%;" border="1"><colgroup><col style="width: 50.0684%;"><col style="width: 49.9316%;"></colgroup>
 <tbody>
-<tr style="height: 22.4px;">
-<td style="height: 22.4px;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>Наименование</div>
-</div>
+<tr>
+<td style="text-align: center;" colspan="2"><span id="docs-internal-guid-ad894d3b-7fff-ae46-e1b2-d403c89a79c5">Активный радиолокационный канал</span></td>
+</tr>
+<tr>
+<td><span id="docs-internal-guid-96dc2459-7fff-ccf1-fdd5-4252e2a3ff08">Диапазон частот</span></td>
+<td style="text-align: center;"><span id="docs-internal-guid-3d8200d8-7fff-1a76-0a09-4208dc4aeb3b">X (I)</span></td>
+</tr>
+<tr>
+<td>
+<p><span id="docs-internal-guid-d2eddaaf-7fff-2160-92ab-658101912c5c">Разрешающая способность:</span></p>
+<p><span id="docs-internal-guid-95eee740-7fff-4315-eb81-b37d3acc2fd8">по дальности (на шкале 0,5 мили), м</span></p>
+<p><span id="docs-internal-guid-08ebc86d-7fff-6481-9155-9e8e1b0918a6">по пеленгу, град</span></p>
 </td>
-<td style="height: 22.4px; text-align: center;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>РЛС ШТОРМ</div>
-</div>
+<td style="text-align: center;">
+<p>&nbsp;</p>
+<p>25</p>
+<p>8</p>
 </td>
 </tr>
-<tr style="height: 22.4px;">
-<td style="height: 22.4px;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>Размер антенны (ft)</div>
-</div>
-</td>
-<td style="height: 22.4px; text-align: center;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>4/6</div>
-</div>
-</td>
+<tr>
+<td><span id="docs-internal-guid-eb038850-7fff-1117-3ea5-aa09236f984f">Дальность обнаружения целей (в зависимости от ЭПР целей), км</span></td>
+<td style="text-align: center;">18</td>
 </tr>
-<tr style="height: 22.4px;">
-<td style="height: 22.4px;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>Скорость вращения(Об/мин)</div>
-</div>
-</td>
-<td style="height: 22.4px; text-align: center;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>24/48</div>
-</div>
-</td>
+<tr>
+<td><span id="docs-internal-guid-b6cd6051-7fff-2df9-920d-0e75f8017e94">Автосопровождение целей по дальности</span></td>
+<td style="text-align: center;">+</td>
 </tr>
-<tr style="height: 22.4px;">
-<td style="height: 22.4px;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>Мощность передатчика (Вт)</div>
-</div>
-</td>
-<td style="height: 22.4px; text-align: center;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>300</div>
-</div>
-</td>
+<tr>
+<td><span id="docs-internal-guid-638e0e44-7fff-4b49-3e19-3a27871f68a1">Автоматическая индикация и сигнализация о целях, опасных в навигационном отношении</span></td>
+<td style="text-align: center;">+</td>
 </tr>
-<tr style="height: 22.4px;">
-<td style="height: 22.4px;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>Разрешающая способность по&nbsp;дальности (м)</div>
-</div>
-</td>
-<td style="height: 22.4px; text-align: center;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>7.5</div>
-</div>
-</td>
+<tr>
+<td><span id="docs-internal-guid-131d3a7c-7fff-6770-10b8-cc2362d960df">Проигрывание маневра расхождения изменением курса и скорости для избежания столкновения</span></td>
+<td style="text-align: center;">+</td>
 </tr>
-<tr style="height: 22.4px;">
-<td style="height: 22.4px;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>Разрешающая способность по углу&nbsp;(град.)</div>
-</div>
-</td>
-<td style="height: 22.4px; text-align: center;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>1.8/1.2</div>
-</div>
-</td>
+<tr>
+<td style="text-align: center;" colspan="2"><span id="docs-internal-guid-05f6a022-7fff-ad88-fe98-eaa56b7d6ea7">Пассивный радиолокационный канал</span></td>
 </tr>
-<tr style="height: 22.4px;">
-<td style="height: 22.4px;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>Комплект поставки</div>
-</div>
-</td>
-<td style="height: 22.4px; text-align: center;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>РЛС + индикатор + пульт управления</div>
-<div>+ блок обработки</div>
-</div>
-</td>
+<tr>
+<td><span id="docs-internal-guid-91dba5f4-7fff-c756-b230-0a4df5776b11">Диапазон принимаемых частот</span></td>
+<td style="text-align: center;"><span id="docs-internal-guid-943bdd1f-7fff-72fa-e2ac-bf02abf68b4e">Дециметровый и сантиметровый (сплошное перекрытие)</span></td>
 </tr>
-<tr style="height: 22.4px;">
-<td style="height: 22.4px;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>Минимальная дальность&nbsp;обнаружения морских целей,&nbsp;(м)</div>
-</div>
-</td>
-<td style="height: 22.4px; text-align: center;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>Согласно таблице 5.7.5 правил по&nbsp;оборудованию морских судов РМРС,&nbsp;15</div>
-</div>
-</td>
+<tr>
+<td><span id="docs-internal-guid-3405ccff-7fff-e6b9-a040-ef01060d113c">Виды принимаемых сигналов</span></td>
+<td style="text-align: center;"><span id="docs-internal-guid-a080f26b-7fff-917d-b615-3f78ad90069f">Импульсные и сложные с различными видами модуляции</span></td>
 </tr>
-<tr style="height: 22.4px;">
-<td style="height: 22.4px;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>Питание (В)</div>
-</div>
-</td>
-<td style="height: 22.4px; text-align: center;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>220 AC / 24 DC</div>
-</div>
-</td>
+<tr>
+<td><span id="docs-internal-guid-3405ccff-7fff-e6b9-a040-ef01060d113c"><span id="docs-internal-guid-562bf465-7fff-27ff-94f8-4d01e183ad4f">Чувствительность приемного устройства при приеме импульсных и непрерывных сигналов, Вт</span></span></td>
+<td style="text-align: center;"><span id="docs-internal-guid-d37fb57d-7fff-b0a2-ec8b-569675bdc9d2">10</span><sup>-10</sup> &ndash; 10<sup>-11</sup></td>
 </tr>
-<tr style="height: 22.4px;">
-<td style="height: 22.4px;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>Видеоданные</div>
-</div>
-</td>
-<td style="height: 22.4px; text-align: center;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>Asterix 240</div>
-</div>
-</td>
+<tr>
+<td><span id="docs-internal-guid-3405ccff-7fff-e6b9-a040-ef01060d113c"><span id="docs-internal-guid-99b4dceb-7fff-4809-8ef2-7f4dc155ad00">Виды обзора пространства</span></span></td>
+<td style="text-align: center;"><span id="docs-internal-guid-075091b2-7fff-cddb-faa5-c67d68f18c55">Круговой, секторный</span></td>
 </tr>
-<tr style="height: 22.4px;">
-<td style="height: 22.4px;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>Взаимодействие с системами судна,&nbsp;согласно требованиям РМРС</div>
-</div>
-</td>
-<td style="height: 22.4px; text-align: center;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>да</div>
-</div>
-</td>
+<tr>
+<td><span id="docs-internal-guid-3405ccff-7fff-e6b9-a040-ef01060d113c"><span id="docs-internal-guid-e42736cd-7fff-661a-226f-d50f53803e62">Электропитание от сети постоянного тока, В</span></span></td>
+<td style="text-align: center;"><span id="docs-internal-guid-1da376bc-7fff-e923-78a0-bdb81a22f015">175 - 320</span></td>
 </tr>
-<tr style="height: 22.4px;">
-<td style="height: 22.4px;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>Навигационное ПО</div>
-</div>
-</td>
-<td style="height: 22.4px; text-align: center;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>да</div>
-</div>
-</td>
+<tr>
+<td><span id="docs-internal-guid-3405ccff-7fff-e6b9-a040-ef01060d113c"><span id="docs-internal-guid-e42736cd-7fff-661a-226f-d50f53803e62"><span id="docs-internal-guid-06ed7f93-7fff-8a64-c0a7-84b7bbb63d1c">Потребляемая мощность, кВт</span></span></span></td>
+<td style="text-align: center;"><span id="docs-internal-guid-cd314d25-7fff-0213-174b-f9fd31635333">3,5</span></td>
 </tr>
 </tbody>
 </table>',
-            'characteristics_en' => '<table style="border-collapse: collapse; width: 100%;" border="1"><colgroup><col style="width: 50%;"><col style="width: 50%;"></colgroup>
+            'characteristics_en' => '<table style="border-collapse: collapse; width: 100%;" border="1"><colgroup><col style="width: 50.0684%;"><col style="width: 49.9316%;"></colgroup>
 <tbody>
 <tr>
-<td>
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>Designation</div>
-</div>
-</td>
-<td style="text-align: center;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div style="text-align: center;">Radar STORM</div>
-</div>
-</td>
+<td style="text-align: center;" colspan="2"><span id="docs-internal-guid-ad894d3b-7fff-ae46-e1b2-d403c89a79c5"><span id="docs-internal-guid-60329a44-7fff-3914-1cd6-5fb962b3f4c3">Active radar channel</span></span></td>
+</tr>
+<tr>
+<td><span id="docs-internal-guid-96dc2459-7fff-ccf1-fdd5-4252e2a3ff08"><span id="docs-internal-guid-c3607e47-7fff-7254-4aa4-bb64e0b4f5e6">Frequency range</span></span></td>
+<td style="text-align: center;"><span id="docs-internal-guid-3d8200d8-7fff-1a76-0a09-4208dc4aeb3b">X (I)</span></td>
 </tr>
 <tr>
 <td>
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>Dimensions of antenna (ft)</div>
-</div>
+<p><span id="docs-internal-guid-74c2c2ad-7fff-9974-dcec-b9cae53b6808">Resolution:</span></p>
+<p><span id="docs-internal-guid-a000597e-7fff-c033-98cc-37f7ede35953">on range (on scale 0.5 miles), m</span></p>
+<p><span id="docs-internal-guid-277fcb3a-7fff-0100-5443-88514872fbbe">on bearing, degrees</span></p>
 </td>
 <td style="text-align: center;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>4/6</div>
-</div>
+<p>&nbsp;</p>
+<p>25</p>
+<p>8</p>
 </td>
 </tr>
 <tr>
-<td>
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>Rotation speed (rpm)</div>
-</div>
-</td>
-<td style="text-align: center;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>24/48</div>
-</div>
-</td>
+<td><span id="docs-internal-guid-eb038850-7fff-1117-3ea5-aa09236f984f"><span id="docs-internal-guid-624c3d33-7fff-28ca-6871-a140982dc717">Target detection distance (depending on RCS), km</span></span></td>
+<td style="text-align: center;">18</td>
 </tr>
 <tr>
-<td>
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>Power of transmitter (W)</div>
-</div>
-</td>
-<td style="text-align: center;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>300</div>
-</div>
-</td>
+<td><span id="docs-internal-guid-b6cd6051-7fff-2df9-920d-0e75f8017e94"><span id="docs-internal-guid-ea1b7d49-7fff-6685-292e-f4b8e966973b">Automatic target tracking on distance</span></span></td>
+<td style="text-align: center;">+</td>
 </tr>
 <tr>
-<td>
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>Resolution on distance (m)</div>
-</div>
-</td>
-<td style="text-align: center;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>7.5</div>
-</div>
-</td>
+<td><span id="docs-internal-guid-638e0e44-7fff-4b49-3e19-3a27871f68a1"><span id="docs-internal-guid-103ed8a1-7fff-8fab-db54-1ae54c2dc0e5">Automatic indication and signaling on targets, dangerous for navigation</span></span></td>
+<td style="text-align: center;">+</td>
 </tr>
 <tr>
-<td>
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>Resolution on angle (deg)</div>
-</div>
-</td>
-<td style="text-align: center;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>1.8/1.2</div>
-</div>
-</td>
+<td><span id="docs-internal-guid-131d3a7c-7fff-6770-10b8-cc2362d960df"><span id="docs-internal-guid-56d88241-7fff-d0a4-bba4-45359d201820">Displaying of divergence maneuver by changing course and speed to avid collision</span></span></td>
+<td style="text-align: center;">+</td>
 </tr>
 <tr>
-<td>
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>Set of supply</div>
-</div>
-</td>
-<td style="text-align: center;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>Radar + display + control console +</div>
-<div>processing unit</div>
-</div>
-</td>
+<td style="text-align: center;" colspan="2"><span id="docs-internal-guid-05f6a022-7fff-ad88-fe98-eaa56b7d6ea7"><span id="docs-internal-guid-8032864f-7fff-006e-1379-4b335ec120cd">Passive radar channel</span></span></td>
 </tr>
 <tr>
-<td>
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>Minimum distance for marine targets</div>
-<div>detection, (m)</div>
-</div>
-</td>
-<td style="text-align: center;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>In accordance with table 5.7.5 of the Rules</div>
-<div>for equipping of marine vessels of Russian</div>
-<div>Maritime Register of shipping,</div>
-<div>15</div>
-</div>
-</td>
+<td><span id="docs-internal-guid-91dba5f4-7fff-c756-b230-0a4df5776b11"><span id="docs-internal-guid-10c6fc2e-7fff-369a-0840-e2e8c8bbfd79">Band of received signals</span></span></td>
+<td style="text-align: center;"><span id="docs-internal-guid-943bdd1f-7fff-72fa-e2ac-bf02abf68b4e"><span id="docs-internal-guid-7574edc2-7fff-fb73-0a5a-b3994ef79761">HF, UHF (full coverage)</span></span></td>
 </tr>
 <tr>
-<td>
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>Power supply (V)</div>
-</div>
-</td>
-<td style="text-align: center;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>220 AC / 24 DC</div>
-</div>
-</td>
+<td><span id="docs-internal-guid-3405ccff-7fff-e6b9-a040-ef01060d113c"><span id="docs-internal-guid-525aa9cc-7fff-65c4-2727-2ac945c0293b">Type of received signals</span></span></td>
+<td style="text-align: center;"><span id="docs-internal-guid-a080f26b-7fff-917d-b615-3f78ad90069f"><span id="docs-internal-guid-191be902-7fff-1e50-01d7-699ddcffd1b8">Pulse and complex with various types of modulation</span></span></td>
 </tr>
 <tr>
-<td>
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>Video data</div>
-</div>
-</td>
-<td style="text-align: center;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>Asterix 240</div>
-</div>
-</td>
+<td><span id="docs-internal-guid-3405ccff-7fff-e6b9-a040-ef01060d113c"><span id="docs-internal-guid-562bf465-7fff-27ff-94f8-4d01e183ad4f"><span id="docs-internal-guid-01fce276-7fff-8979-2ad6-63b562b28a78">Sensitivity of receiver when receiving pulse and continuous signals, W</span></span></span></td>
+<td style="text-align: center;"><span id="docs-internal-guid-d37fb57d-7fff-b0a2-ec8b-569675bdc9d2">10</span><sup>-10</sup> &ndash; 10<sup>-11</sup></td>
 </tr>
 <tr>
-<td>
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>Interfacing with ship systems, as per</div>
-<div>requirements of the Russian Maritime</div>
-<div>Register of shipping</div>
-</div>
-</div>
-</div>
-</td>
-<td style="text-align: center;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>Yes</div>
-</div>
-</td>
+<td><span id="docs-internal-guid-3405ccff-7fff-e6b9-a040-ef01060d113c"><span id="docs-internal-guid-99b4dceb-7fff-4809-8ef2-7f4dc155ad00"><span id="docs-internal-guid-f19021da-7fff-bd27-e6a5-ffbcad2d3eef">Types of scanning</span></span></span></td>
+<td style="text-align: center;"><span id="docs-internal-guid-075091b2-7fff-cddb-faa5-c67d68f18c55"><span id="docs-internal-guid-71176fbb-7fff-4c6d-3efe-734391023da8">All-round, sector</span></span></td>
 </tr>
 <tr>
-<td>
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>Navigational software</div>
-</div>
-</div>
-</div>
-</div>
-</div>
-</td>
-<td style="text-align: center;">
-<div class="glass_k7icd9BmH9aFeHLwMLjF">
-<div>Yes</div>
-</div>
-</td>
+<td><span id="docs-internal-guid-3405ccff-7fff-e6b9-a040-ef01060d113c"><span id="docs-internal-guid-e42736cd-7fff-661a-226f-d50f53803e62"><span id="docs-internal-guid-3d2a372d-7fff-e4c4-9a34-07bf6dce5c1f">Power supply from the DC mains, V</span></span></span></td>
+<td style="text-align: center;"><span id="docs-internal-guid-1da376bc-7fff-e923-78a0-bdb81a22f015">175 - 320</span></td>
+</tr>
+<tr>
+<td><span id="docs-internal-guid-3405ccff-7fff-e6b9-a040-ef01060d113c"><span id="docs-internal-guid-e42736cd-7fff-661a-226f-d50f53803e62"><span id="docs-internal-guid-06ed7f93-7fff-8a64-c0a7-84b7bbb63d1c"><span id="docs-internal-guid-378c44ea-7fff-cd3a-a10f-ef6ddb01afd7">Power consumption, kW</span></span></span></span></td>
+<td style="text-align: center;"><span id="docs-internal-guid-cd314d25-7fff-0213-174b-f9fd31635333">3,5</span></td>
 </tr>
 </tbody>
 </table>',
